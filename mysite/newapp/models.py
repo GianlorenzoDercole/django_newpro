c8141ee134a4656e4421902c38f2b411b5f2dd9f
GianlorenzoDercole <johndercole01@gmail.com>
2023-02-02 16:31:42 -0800

    added movie_list view see views.py line 5 and urls.py line 22

--- mysite/newapp/models.py
+++ mysite/newapp/models.py
@@ -4,5 +4,9 @@ from django.db import models
 
 
 class Movies(models.Model):
+
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=200)
     rating = models.FloatField()
